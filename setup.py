be2b311980111675d60517d8456b29b9ab207500
maxyshaa <maxysha95@mail.ru>
2020-03-21 23:55:37 +0300

    cleaning

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='lm-pkg-Maksimova',
+      version='0.0.1',
+      description='Package with linear models for animals value prediction',
+      url='https://github.com/maxyshaa/cow_intership.git',
+      author='Ksenia Maximova',
+      author_email='maxysha95@gmail.com',
+      packages= ['lm'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+                  ],
+      python_requires='>=3.6',
+
+)
