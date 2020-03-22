389ad67c40db88e741e92e39d084572fd342701a
maxyshaa <maxysha95@mail.ru>
2020-03-23 02:54:47 +0300

    adding install requires

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
+```python
 from setuptools import setup
 
-setup(name='lm-pkg-Maksimova',
-      version='0.0.1',
-      description='Package with linear models for animals value prediction',
-      url='https://github.com/maxyshaa/cow_intership.git',
-      author='Ksenia Maximova',
-      author_email='maxysha95@gmail.com',
-      packages= ['blup_animals'],
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: OS Independent",
-                  ],
-      python_requires='>=3.6',
-
+setup(
+    name='lm-pkg-Maksimova', version='0.0.1',
+    description='Package with linear models for animals value prediction',
+    url='https://github.com/maxyshaa/cow_intership.git',
+    author='Ksenia Maximova',
+    author_email='maxysha95@gmail.com',
+    packages= ['blup_animals'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pandas>=0.24.2",
+        "numpy>=1.16.2"
+    ]
 )
+```
