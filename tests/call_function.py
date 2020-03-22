4a49392758100e2e426a1d2a4435a6eba2e4016a
maxyshaa <maxysha95@mail.ru>
2020-03-22 19:50:06 +0300

    add table

--- tests/call_function.py
+++ tests/call_function.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from sklearn.datasets import load_boston
-
 
 dataset = load_boston()
 X = dataset.data
---
+++ tests/test.py
@@ -0,0 +1,14 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def test___init__(self):
+        # 
+        assert False # TODO: implement your test here
+
+    def test_function(self):
+        # 
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
+
